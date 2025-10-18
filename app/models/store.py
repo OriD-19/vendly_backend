75fecda7fa690715c3e27e2d7f6861bd441898aa
Fernando José Fuentes Castillo <fernandocastillo.8@hotmail.com>
2025-10-18 16:59:30 -0600

    wip: improving the store information

--- app/models/store.py
+++ app/models/store.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -9,8 +9,18 @@ class Store(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(80), nullable=False, unique=True)
+
+    # store information
+    phone = Column(String(20), nullable=True)
+    email = Column(String(120), nullable=True)
     store_location = Column(String(120), nullable=False)
     type = Column(String(50), nullable=False)
+    
+    # location images
+    # stored in an S3 bucket (ideally, lol), this field contains the URL/path to the image
+    profile_image = Column(String(255), nullable=True)
 
     created_at = Column(DateTime(), nullable=False, default=datetime.now)
     updated_at = Column(DateTime(), nullable=False, onupdate=datetime.now)
+
+    products = relationship("Product", back_populates="store")
